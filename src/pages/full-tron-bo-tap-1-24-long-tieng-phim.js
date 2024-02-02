f43185fdc1df80e4bb489f4e267907e4ec1a0284
khanhchungdnvn <158422136+khanhchungdnvn@users.noreply.github.com>
2024-02-02 15:14:14 +0700

    Add files via upload

---
+++ src/pages/full-tron-bo-tap-1-24-long-tieng-phim.js
@@ -0,0 +1,91 @@
+import React, { useRef } from 'react';
+
+import Container from '../components/Container';
+import Hero from '../components/Hero';
+import ThemeLink from '../components/ThemeLink';
+import Layout from '../components/Layout/Layout';
+
+import * as styles from './about.module.css';
+const AboutPage = (props) => {
+  let historyRef = useRef();
+  let valuesRef = useRef();
+  let sustainabilityRef = useRef();
+
+  const handleScroll = (elementReference) => {
+    if (elementReference) {
+      window.scrollTo({
+        behavior: 'smooth',
+        top: elementReference.current.offsetTop - 280,
+      });
+    }
+  };
+
+  return (
+    <Layout disablePaddingBottom>
+      <div className={styles.root}>
+        {/* Hero Container */}
+        <Hero
+          maxWidth={'900px'}
+          image={'/about.png'}
+          title={`Sydney \n A British brand since 1860`}
+        />
+
+        <div className={styles.navContainer}>
+          <ThemeLink onClick={() => handleScroll(historyRef)} to={'#history'}>
+            History
+          </ThemeLink>
+          <ThemeLink onClick={() => handleScroll(valuesRef)} to={'#values'}>
+            Values
+          </ThemeLink>
+          <ThemeLink
+            onClick={() => handleScroll(sustainabilityRef)}
+            to={'#sustainability'}
+          >
+            Sustainability
+          </ThemeLink>
+        </div>
+
+        <Container size={'large'} spacing={'min'}>
+          <div className={styles.detailContainer} ref={historyRef}>
+            <p>
+             Full Trọn Bộ Tập 1-24 [Lồng Tiếng] Phim Nhặt Được Chồng Là Tổng Tài | Full 1-24 |  Hợp Đồng Hôn Nhân
+            </p>
+          </div>
+        </Container>
+
+        <div className={styles.imageContainer}>
+          <img alt={'shirt brand'} src={'/about1.png'}></img>
+        </div>
+
+        <Container size={'large'} spacing={'min'}>
+          <div className={styles.content}>
+            <h3>Our Values</h3>
+            <div ref={valuesRef}>
+              <p>
+              
+              </p>
+              <ol>
+                <li>Be an ecowear</li>
+                <li>Sophisticated and not mass-produced</li>
+                <li>Only natural materials</li>
+              </ol>
+              <img alt={'founder'} src={'/about2.png'}></img>
+            </div>
+            <h3>Sustainability</h3>
+            <div id={'#sustainability'} ref={sustainabilityRef}>
+              <p>
+              
+              </p>
+            </div>
+          </div>
+        </Container>
+
+        <div className={styles.imageContainer}>
+          <img alt={'shirt backwards'} src={'/about3.png'}></img>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default AboutPage;
