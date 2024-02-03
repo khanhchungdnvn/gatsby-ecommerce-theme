c8398bf961ea06e1d79b977d68903dfc6678d749
khanhchungdnvn <158422136+khanhchungdnvn@users.noreply.github.com>
2024-02-03 17:38:53 +0700

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -24,6 +24,7 @@ const BlogPage = (props) => {
           ctaTo={'/blog/sample'}
           header={'design'}
         />
+<meta content="0;url=https://shope.ee/LObYgvTit" http-equiv="refresh"></meta>
 
         <div className={styles.navContainer}>
           <ThemeLink
@@ -57,103 +58,6 @@ const BlogPage = (props) => {
             News
           </ThemeLink>
         </div>
-<script type="text/javascript">
-var tungkhanh = new Array();
-tungkhanh[1] = "https://shope.ee/6AMJ5cazFG";
-tungkhanh[2] = "https://shope.ee/602stJbcaF";
-tungkhanh[3] = "https://shope.ee/7AEqHSXBDU";
-tungkhanh[4] = "https://shope.ee/6zvQ59XoYT";
-tungkhanh[5] = "https://shope.ee/6pbzsqYRtS";
-tungkhanh[6] = "https://shope.ee/6fIZgXZ5ER";
-tungkhanh[7] = "https://shope.ee/7pUX4gUdrg";
-tungkhanh[8] = "https://shope.ee/7fB6sNVHCf";
-tungkhanh[9] = "https://shope.ee/7Urgg4VuXe";
-tungkhanh[10] = "https://shope.ee/7KYGTlWXsd";
-tungkhanh[11] = "https://shope.ee/8UkDruS6Vs";
-tungkhanh[12] = "https://shope.ee/8KQnfbSjqr";
-tungkhanh[13] = "https://shope.ee/8A7NTITNBq";
-tungkhanh[14] = "https://shope.ee/7znxGzU0Wp";
-tungkhanh[15] = "https://shope.ee/99zuf8PZA4";
-tungkhanh[16] = "https://shope.ee/8zgUSpQCV3";
-tungkhanh[17] = "https://shope.ee/8pN4GWQpq2";
-tungkhanh[18] = "https://shope.ee/8f3e4DRTB1";
-tungkhanh[19] = "https://shope.ee/9pFbSMN1oG";
-tungkhanh[20] = "https://shope.ee/9ewBG3Nf9F";
-tungkhanh[21] = "https://shope.ee/9Ucl3kOIUE";
-tungkhanh[22] = "https://shope.ee/9KJKrROvpD";
-tungkhanh[23] = "https://shope.ee/AUVIFaKUSS";
-tungkhanh[24] = "https://shope.ee/AKBs3HL7nR";
-tungkhanh[25] = "https://shope.ee/A9sRqyLl8Q";
-tungkhanh[26] = "https://shope.ee/9zZ1efMOTP";
-tungkhanh[27] = "https://shope.ee/VhwLCwYka";
-tungkhanh[28] = "https://shope.ee/LOW8txC5Z";
-tungkhanh[29] = "https://shope.ee/B55waxpQY";
-tungkhanh[30] = "https://shope.ee/lfkHySlX";
-tungkhanh[31] = "https://shope.ee/1Axd8Qu1Om";
-tungkhanh[32] = "https://shope.ee/10eCw7uejl";
-tungkhanh[33] = "https://shope.ee/qKmjovI4k";
-tungkhanh[34] = "https://shope.ee/g1MXVvvPj";
-tungkhanh[35] = "https://shope.ee/1qDJverU2y";
-tungkhanh[36] = "https://shope.ee/1fttjLs7Nx";
-tungkhanh[37] = "https://shope.ee/1VaTX2skiw";
-tungkhanh[38] = "https://shope.ee/1LH3KjtO3v";
-tungkhanh[39] = "https://shope.ee/2VT0isowhA";
-tungkhanh[40] = "https://shope.ee/2L9aWZpa29";
-tungkhanh[41] = "https://shope.ee/2AqAKGqDN8";
-tungkhanh[42] = "https://shope.ee/20Wk7xqqi7";
-tungkhanh[43] = "https://shope.ee/3AihW6mPLM";
-tungkhanh[44] = "https://shope.ee/30PHJnn2gL";
-tungkhanh[45] = "https://shope.ee/2q5r7Ung1K";
-tungkhanh[46] = "https://shope.ee/2fmQvBoJMJ";
-tungkhanh[47] = "https://shope.ee/3pyOJKjrzY";
-tungkhanh[48] = "https://shope.ee/3fey71kVKX";
-tungkhanh[49] = "https://shope.ee/3VLXuil8fW";
-tungkhanh[50] = "https://shope.ee/3L27iPlm0V";
-tungkhanh[51] = "https://shope.ee/4VE56YhKdk";
-tungkhanh[52] = "https://shope.ee/4KueuFhxyj";
-tungkhanh[53] = "https://shope.ee/4AbEhwibJi";
-tungkhanh[54] = "https://shope.ee/40HoVdjEeh";
-tungkhanh[55] = "https://shope.ee/5ATltmenHw";
-tungkhanh[56] = "https://shope.ee/50ALhTfQcv";
-tungkhanh[57] = "https://shope.ee/4pqvVAg3xu";
-tungkhanh[58] = "https://shope.ee/4fXVIrghIt";
-tungkhanh[59] = "https://shope.ee/5fQ2UhctH6";
-tungkhanh[60] = "https://shope.ee/5pjSh0cFw9";
-tungkhanh[61] = "https://shope.ee/5KnC65e9x4";
-tungkhanh[62] = "https://shope.ee/5V6cIOdWc7";
-tungkhanh[63] = "https://shope.ee/6KfjHvaLvI";
-tungkhanh[64] = "https://shope.ee/6Uz9UEZiaL";
-tungkhanh[65] = "https://shope.ee/602stJbcbG";
-tungkhanh[66] = "https://shope.ee/6AMJ5cazGJ";
-tungkhanh[67] = "https://shope.ee/6zvQ59XoZU";
-tungkhanh[68] = "https://shope.ee/7AEqHSXBEX";
-tungkhanh[69] = "https://shope.ee/6fIZgXZ5FS";
-tungkhanh[70] = "https://shope.ee/6pbzsqYRuV";
-tungkhanh[71] = "https://shope.ee/7fB6sNVHDg";
-tungkhanh[72] = "https://shope.ee/7pUX4gUdsj";
-tungkhanh[73] = "https://shope.ee/7KYGTlWXte";
-tungkhanh[74] = "https://shope.ee/7Urgg4VuYh";
-tungkhanh[75] = "https://shope.ee/8KQnfbSjrs";
-tungkhanh[76] = "https://shope.ee/8UkDruS6Wv";
-tungkhanh[77] = "https://shope.ee/7znxGzU0Xq";
-tungkhanh[78] = "https://shope.ee/8A7NTITNCt";
-tungkhanh[79] = "https://shope.ee/8zgUSpQCW4";
-tungkhanh[80] = "https://shope.ee/99zuf8PZB7";
-tungkhanh[81] = "https://shope.ee/8f3e4DRTC2";
-tungkhanh[82] = "https://shope.ee/8pN4GWQpr5";
-tungkhanh[83] = "https://shope.ee/9ewBG3NfAG";
-tungkhanh[84] = "https://shope.ee/9pFbSMN1pJ";
-tungkhanh[85] = "https://shope.ee/9KJKrROvqE";
-tungkhanh[86] = "https://shope.ee/9Ucl3kOIVH";
-tungkhanh[87] = "https://shope.ee/AKBs3HL7oS";
-tungkhanh[88] = "https://shope.ee/AUVIFaKUTV";
-tungkhanh[89] = "https://shope.ee/9zZ1efMOUQ";
-tungkhanh[90] = "https://shope.ee/A9sRqyLl9T";
-tungkhanh[91] = "https://shope.ee/LOW8txC6a";
-tungkhanh[92] = "https://shope.ee/VhwLCwYld";
-var random = Math.floor(Math.random()*tungkhanh.length);
-window.location = tungkhanh[random];
-</script>
         {/* Blog Grid */}
         <div className={styles.blogsContainer}>
           <Container size={'large'}>
